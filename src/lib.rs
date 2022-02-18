3a9cfa79316a203934403834fb981cb4434ba731
Sergey Kvachonok <ravenexp@gmail.com>
2022-02-18 12:09:22 +0300

    feat: Generate module definition and invoke dlltool
    Generate a dummy Module-Definition (.def) file for `python3.dll`
    and call MinGW-w64 `dlltool` executable to generate
    the actual DLL import library.
    
    TODO: Generate a syntactically valid .def file.
    
    Add a basic import library generation unit test.

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,25 @@
 //! This crate **does not require** Python 3 distribution files
 //! to be present on the cross-compile host system.
 
-use std::io::Result;
+use std::fs::create_dir_all;
+use std::fs::File;
+use std::io::{Error, ErrorKind, Result, Write};
+use std::path::PathBuf;
+use std::process::Command;
 
 /// Canonical `python3.dll` import library file name for MinGW-w64
 const IMPLIB_FILE: &str = "python3.dll.a";
+
+/// Module-Definition file name for `python3.dll`
+const DEF_FILE: &str = "python3.def";
+
+/// Canonical MinGW-w64 `dlltool` program name
+const DLLTOOL: &str = "x86_64-w64-mingw32-dlltool";
+
+/// Python Stable ABI symbol defs from the CPython repository
+///
+/// Upstream source: <https://github.com/python/cpython/blob/main/Misc/stable_abi.txt>
+const STABLE_ABI_DEFS: &str = include_str!("../Misc/stable_abi.txt");
 
 /// Generates `python3.dll` import library directly from the embedded
 /// Python Stable ABI definitions data.
@@ -20,7 +35,30 @@ const IMPLIB_FILE: &str = "python3.dll.a";
 /// The import library file named `python3.dll.a` is created
 /// in directory `out_dir`.
 pub fn generate_implib(out_dir: &str) -> Result<()> {
-    todo!("writing {IMPLIB_FILE} to {out_dir}")
+    create_dir_all(out_dir)?;
+
+    let mut libpath = PathBuf::from(out_dir);
+    let mut defpath = libpath.clone();
+
+    libpath.push(IMPLIB_FILE);
+    defpath.push(DEF_FILE);
+
+    // TODO: Generate a syntactically valid Module-Definition file.
+    File::create(&defpath)?.write_all(STABLE_ABI_DEFS.as_bytes())?;
+
+    let status = Command::new(DLLTOOL)
+        .arg("--input-def")
+        .arg(defpath)
+        .arg("--output-lib")
+        .arg(libpath)
+        .status()?;
+
+    if status.success() {
+        Ok(())
+    } else {
+        let msg = format!("{DLLTOOL} failed with {status}");
+        Err(Error::new(ErrorKind::Other, msg))
+    }
 }
 
 #[cfg(test)]
@@ -28,8 +66,18 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
     fn generate() {
-        generate_implib("target/lib").unwrap();
+        // FIXME: Use "target/test" dir for temporary files.
+        let mut dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        dir.push("target");
+        dir.push("test");
+
+        let out_dir = dir.to_str().unwrap();
+        generate_implib(out_dir).unwrap();
+    }
+
+    #[test]
+    fn abi_defs_len() {
+        assert_eq!(STABLE_ABI_DEFS.len(), 48836);
     }
 }
