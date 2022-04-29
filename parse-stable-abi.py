131f4bf40a2947e4ec8672c91a3539d2cd13ee9b
Adam Reichold <adam.reichold@t-online.de>
2022-04-29 18:08:28 +0200

    feat: Switch to parsing the TOML instead of the text file.

--- parse-stable-abi.py
+++ parse-stable-abi.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
-# Parses Python Stable ABI symbol definitions from the manifest in the CPython repository located at https://github.com/python/cpython/blob/main/Misc/stable_abi.txt
+# Parses Python Stable ABI symbol definitions from the manifest in the CPython repository located at https://github.com/python/cpython/blob/main/Misc/stable_abi.toml
 # and produces a definition file following the format described at https://docs.microsoft.com/en-us/cpp/build/reference/module-definition-dot-def-files.
 import sys
+import tomli
+
+stable_abi = tomli.load(sys.stdin.buffer)
 
 print("LIBRARY python3.dll")
 print("EXPORTS")
 
 count = 0
 
-for line in sys.stdin:
-    if line.startswith("function"):
-        is_data = False
-    elif line.startswith("data"):
-        is_data = True
-    else:
-        continue
-
+for function in stable_abi["function"].keys():
+    print(function)
     count += 1
-    name = line.split()[1]
 
-    if is_data:
-        print(f"{name} DATA")
-    else:
-        print(name)
+for data in stable_abi["data"].keys():
+    print(f"{data} DATA")
+    count += 1
 
-assert count >= 859
+assert count >= 861
